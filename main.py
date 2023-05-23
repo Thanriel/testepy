c46683f623df8834657cc3e22b9d11e0e11a034c
thanriel <thalesemanonelrsantos@gmail.com>
2023-05-23 18:58:23 -0300

    Main commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Olá, Azure App Service com Python!"
+    return "Olá, Azure App Service com Python, teste!"
 
 if __name__ == "__main__":
     app.run()
